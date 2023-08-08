34faf9c6d18c49e87b9d36acb92779a3f9707873
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-08-08 19:08:05 +0200

    update color values

--- color_pose_estimation/color_pose_estimation/detect_color_scene.py
+++ color_pose_estimation/color_pose_estimation/detect_color_scene.py
@@ -9,10 +9,10 @@ YELLOW_HIGH = np.array([35, 255, 255])
 BLUE_LOW = np.array([100, 70, 80])
 BLUE_HIGH = np.array([130, 255, 255])
 
-RED1_LOW = np.array([0, 120, 70])
+RED1_LOW = np.array([0, 60, 70])
 RED1_HIGH = np.array([10, 255, 255])
 
-RED2_LOW = np.array([160, 120, 80])
+RED2_LOW = np.array([160, 60, 70])
 RED2_HIGH = np.array([180, 255, 255])
 
 GREEN_LOW = np.array([40, 30, 110])
@@ -21,10 +21,11 @@ GREEN_HIGH = np.array([80, 255, 255])
 # COLOR_RANGES = (YELLOW_LOW, YELLOW_HIGH, BLUE_LOW, BLUE_HIGH, RED_LOW, RED_HIGH, GREEN_LOW, GREEN_HIGH)
 # COLOR_NAMES = ("Yellow", "Blue", "Red", "Green")
 
-COLOR_RANGES = {"Yellow": (YELLOW_LOW, YELLOW_HIGH), 
-                "Blue": (BLUE_LOW, BLUE_HIGH),
-                "Green": (GREEN_LOW, GREEN_HIGH)}
-
+COLOR_RANGES = {
+    "Yellow": (np.array([15, 60, 100]), np.array([35, 255, 255])),
+    "Blue": (np.array([110, 60, 70]), np.array([130, 255, 255])),
+    "Green": (np.array([40, 60, 120]), np.array([80, 255, 255])),
+}
     
 # define the color range that will be detected in the image 
 def define_color_range(low, high, img):
@@ -67,7 +68,6 @@ def filter_largest_rectangles(cnts):
 # detect rectangular color ranges in the image  
 def detect(img):
     color_array = [];
-    
     red_mask1 = define_color_range(RED1_LOW, RED1_HIGH, img)
     red_mask2 = define_color_range(RED2_LOW, RED2_HIGH, img)
     red_mask = cv2.bitwise_or(red_mask1, red_mask2)
